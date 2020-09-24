0ca6927bf7d2304f749a9f80eadf4c9c3173e2cd
catherinestorm <43492341+catherinestorm@users.noreply.github.com>
2020-09-24 09:40:38 +0100

    removed get_r_from_lor for progression

--- R/mr_analysis_conservative_r2_0.001.R
+++ R/mr_analysis_conservative_r2_0.001.R
@@ -54,8 +54,6 @@ if (grepl("nalls2014", unique(dat0.001$outcome), fixed = TRUE) == TRUE) {
   dat0.001$r.outcome <- get_r_from_lor(dat0.001$beta.outcome, af = dat0.001$eaf.outcome, ncase = 13708, ncontrol = 95282, prevalence = 0.01)
 } else if (grepl("nalls2019", unique(dat0.001$outcome), fixed = TRUE) == TRUE) {
   dat0.001$r.outcome <- get_r_from_lor(dat0.001$beta.outcome, af = dat0.001$eaf.outcome, ncase = 8036, ncontrol = 5803, prevalence = 0.01)
-} else if (grepl("surv_", unique(dat0.001$outcome), fixed = TRUE) == TRUE) {
-  dat0.001$r.outcome <- get_r_from_lor(dat0.001$beta.outcome, af = dat0.001$eaf.outcome, ncase = 2047, ncontrol = 2046, prevalence = 0.01)
 }
 
 steiger_nalls0.001 <- directionality_test(dat0.001)
